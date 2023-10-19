6343fa708b3ab079b02e06b283e4e6d3376efa17
fmg75 <f.mg@outlook.com>
2023-10-19 12:43:42 -0300

    bug PNG

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from facenet_pytorch import MTCNN, InceptionResnetV1
 from PIL import Image
 import pickle
+import io
 
 # Crear una instancia del modelo InceptionResnetV1 y MTCNN
 model = InceptionResnetV1(pretrained='vggface2').eval()
@@ -30,6 +31,15 @@ uploaded_image = st.file_uploader("Cargar una imagen", type=["jpg", "png", "jpeg
 
 if uploaded_image is not None:
     img = Image.open(uploaded_image)
+
+    # Verificar si la imagen está en formato PNG y convertir a JPG si es necesario
+    if img.format == "PNG":
+        jpg_io = io.BytesIO()  # Crear un objeto BytesIO para guardar la imagen en memoria
+        img = img.convert("RGB")  # Convertir a modo RGB (requerido para guardar como JPG)
+        img.save(jpg_io, format="JPEG")  # Guardar la imagen en el objeto BytesIO en formato JPG
+        jpg_io.seek(0)  # Colocar el puntero de lectura al inicio del objeto BytesIO
+        img = Image.open(jpg_io)  # Abrir la imagen en formato JPG desde el objeto BytesIO
+
     st.image(img, caption="Imagen cargada", use_column_width=True)
 
     if st.button("Reconocer Rostro"):
@@ -45,4 +55,5 @@ if uploaded_image is not None:
                 st.write("No se encontró ningún rostro en la imagen.")
         else:
             st.write("Por favor, cargue una imagen válida.")
+
 
